a9b756527e9c95b84488bf8031f4233640764be2
Nicole Osborn <nicole.y.osborn@gmail.com>
2020-10-19 17:27:57 -0700

    added logging for dbEnv

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -2,6 +2,5 @@ const knex = require('knex');
 
 const knexConfig = require('../knexfile.js');
 const dbEnv = process.env.DB_ENV 
-// || 'development'
-
+console.log("dbEnv", dbEnv)
 module.exports = knex(knexConfig[dbEnv]);
