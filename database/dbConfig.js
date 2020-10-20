a62dfac150756b7f815b6067394b0bb5b3fec7e0
Nicole Osborn <nicole.y.osborn@gmail.com>
2020-10-19 18:17:18 -0700

    update knex file

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -2,8 +2,5 @@ const knex = require('knex');
 
 const knexConfig = require('../knexfile.js');
 const dbEnv = process.env.DB_ENV 
-console.log("dbEnv", dbEnv)
-console.log("knexConfig", knexConfig)
-console.log(process.env.DATABASE_URL, 'process.env.DATABASE_URL')
-console.log(knexConfig['production'])
+//console.log("env", process.env)
 module.exports = knex(knexConfig[dbEnv]);
