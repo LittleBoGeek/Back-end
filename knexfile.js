31e887a6c1527f3024edd7e8eda5577d496bc3c5
Nicole Osborn <nicole.y.osborn@gmail.com>
2020-10-21 19:48:17 -0700

    completed serverspec, all tests pass

--- knexfile.js
+++ knexfile.js
@@ -14,6 +14,13 @@ module.exports = {
       },
     seeds: { directory: './database/seeds'},
   },
+  testing: {
+    client: 'sqlite3',
+    connection: {filename: './database/test.db3'},
+    useNullAsDefault: true,
+    migrations: { directory: './database/migrations'},
+    seeds: { directory: './database/seeds'},
+  },
 
   staging: {
     client: 'postgresql',
